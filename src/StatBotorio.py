f9d2282a9ab550aa14a5f6536e93abac1ab77cb7
Ryan Walker <solarcloud7@yahoo.com>
2019-07-26 17:06:48 -0500

    test commands

--- src/StatBotorio.py
+++ src/StatBotorio.py
@@ -1,19 +1,34 @@
 
 # Work with Python 3.6
 import discord
+import random
+from discord.ext import commands
 from config import Config
 
-client = discord.Client()
+description = '''A bot that shows factorio server statistics for joinandplaycoop.'''
+
+client = commands.Bot(command_prefix='!', description=description)
+
+@client.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 
 @client.event
 async def on_ready():
