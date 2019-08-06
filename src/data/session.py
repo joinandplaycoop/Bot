9457f41271aee5f3d235bea8874bc5008cbf5cfe
Ryan Walker <solarcloud7@yahoo.com>
2019-08-06 15:48:25 -0500

    created reconect funct on session. decorator checks all sql commands for a connection and if fails, auto attemtps 1 time

--- src/data/session.py
+++ src/data/session.py
@@ -3,6 +3,18 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from config import Config
+from datetime import datetime as d
 
 engine = create_engine(Config.cfg.mysql.connectionString)
 Session = sessionmaker(bind=engine)
+connectionStartTime = d.now()
+
+def connect():
+    global engine
+    global Session
+    global connectionStartTime
+    engine = create_engine(Config.cfg.mysql.connectionString)
+    Session = sessionmaker(bind=engine)
+    connectionStartTime = d.now()
+
+connect()
--- src/data/storedProc.py
+++ src/data/storedProc.py
@@ -1,6 +1,22 @@
 from typing import List
 from utilities.diagnostics import executionTime
 from data.session import Session
+import data.session
+from MySQLdb import OperationalError
+
+def sqlFunc():
+    #if DB session disconects, attempt to reconect 1 time.
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except OperationalError as e:
+                if 'MySQL server has gone away' in str(e):
+                    data.session.connect()
+                    return func(*args, **kwargs)
+                print(e)
+        return wrapper
+    return decorator
 
 class PlayersOnline_Result(object):
 
@@ -11,10 +27,14 @@ class PlayersOnline_Result(object):
         self.Status : int = kwargs.get('Status')
         self.Version : int = kwargs.get('Version')
         self.IsResetting : int = kwargs.get('IsResetting')
+
+   
         
     @classmethod
     @executionTime()
-    def execute(cls) -> List['PlayersOnline_Result']:               
+    @sqlFunc()
+    def execute(cls) -> List['PlayersOnline_Result']: 
+
         session = Session()
 
         query = 'call PlayersOnline' 
